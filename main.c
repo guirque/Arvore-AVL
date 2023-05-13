d84d47644272609b06fb98fb18b182805512f92c
Guilherme <guirque@gmail.com>
2023-05-13 15:34:13 -0300

    Balancing Tree on Insertion
    Added checkBalance function to make sure the binary search tree is also an AVL tree. It was implemented in the insert function. main.c was also updated to include an example of a tree which would, normally, be unbalanced, but is fixed by the checkBalance function, with the appropriate insertion function. BF strategy was replaced.

--- main.c
+++ main.c
@@ -5,11 +5,24 @@
 int main()
 {
     tAVL *myTree;
-    tinsert(&myTree, 8);
-    tinsert(&myTree, 5);
-    tinsert(&myTree, 2);
-    tinsert(&myTree, 40);
+    //tinsert(&myTree, 90);
+    //tinsert(&myTree, 100);
+    //tinsert(&myTree, 95);
+    //tinsert(&myTree, 101);
+    //tinsert(&myTree, 42);
+    //tinsert(&myTree, 41);
+    //tinsert(&myTree, 50);
+
+    tinsert(&myTree, 100);
     tinsert(&myTree, 90);
+    tinsert(&myTree, 200);
+    tinsert(&myTree, 125);
+    tinsert(&myTree, 115);
+    tinsert(&myTree, 112);
+    tinsert(&myTree, 210);
+    tinsert(&myTree, 205);
+    tinsert(&myTree, 215);
+    tinsert(&myTree, 220);
 
     printf("Tree (In Order): ");
     tprint(myTree);
--- tAVL.h
+++ tAVL.h
@@ -4,7 +4,6 @@
 typedef struct tAVL
 {
     int data;
-    int BF; //Balancing Factor
     struct tAVL *left;
     struct tAVL *right;
 } tAVL;
@@ -58,8 +57,49 @@ void tprintAllLevels(tAVL *t)
     }
 }
 
+//Check Balance
+tAVL *checkBalance(tAVL *t)
+{
+    if(t == NULL) return NULL;
+
+    int leftHeight = getHeight(t->left);
+    int rightHeight = getHeight(t->right);
+
+    //Rotate right
+    if(leftHeight - rightHeight > 1)
+    {
+        //printf("\n[%d][LeftHeight: %d | RightHeight: %d]", t->data, leftHeight, rightHeight);
+        //printf(" - [Rotating right]");
+        printf("[Rotating Right] because %d is unbalanced.\n", t->data);
+        
+        tAVL *leftSubtree = t->left;        //leftSubtree = 42
+        tAVL *cutPart = leftSubtree->right; //cutPart = null
+        t->left = NULL;                     //Breaking connection: 42 <- 90
+        leftSubtree->right = t;             //42 -> 90
+        leftSubtree->left->right = cutPart; //41 -> null
+
+        return leftSubtree;
+    }
+    //Rotate left
+    else if(rightHeight - leftHeight > 1)
+    {
+        //printf("\n[%d][LeftHeight: %d | RightHeight: %d]", t->data,leftHeight, rightHeight);
+        //printf("- [Rotating left]");
+        printf("[Rotating Left] because %d is unbalanced.\n", t->data);
+        
+        tAVL *rightSubtree = t->right;        //rightSubtree = 100
+        tAVL *cutPart = rightSubtree->left; //cutPart = 95
+        t->right = NULL;                     //Breaking connection: 90 -> 100
+        rightSubtree->left = t;             //90 <- 100
+        rightSubtree->left->right = cutPart; //90 -> 95
+        
+        return rightSubtree;
+    }
+    return t;
+}
+
 //Inserts in Tree
-tAVL *tinsertR(tAVL *t, int value, int currentBF)
+tAVL *tInsertR(tAVL *t, int value)
 {
     //Current BF will hold -1, 0, +1, regarding the difference in height between the right and left subtrees (right - left)
 
@@ -71,14 +111,21 @@ tAVL *tinsertR(tAVL *t, int value, int currentBF)
         leaf->data = value;
         leaf->left = NULL; 
         leaf->right = NULL;
-        leaf->BF = currentBF;
         //Instead of returning the node as it is, return (to father) the new node inserted.
         return leaf;
     }
     //Go right
-    else if(value > t->data) t->right = tinsertR(t->right, value, currentBF);
+    else if(value > t->data)
+    {
+        t->right = tInsertR(t->right, value);
+        return checkBalance(t);
+    }
     //Go left
-    else t->left = tinsertR(t->left, value, currentBF);
+    else 
+    {
+        t->left = tInsertR(t->left, value);
+        return checkBalance(t);
+    }
 
     //End of path (return itself to node father)
     return t;
@@ -86,4 +133,4 @@ tAVL *tinsertR(tAVL *t, int value, int currentBF)
 }
 
 //Void Format
-void tinsert(tAVL **t, int value){*t = tinsertR(*t, value, 0);}
+void tinsert(tAVL **t, int value){*t = tInsertR(*t, value);}
