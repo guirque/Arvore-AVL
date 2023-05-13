cf33c1e65ad5ba43547c3705663fbe68bfa5fa49
Guilherme <guirque@gmail.com>
2023-05-12 22:24:30 -0300

    Adding Basic Functions and Binary Search Tree aspects
    Functions Added:

--- main.c
+++ main.c
@@ -4,5 +4,18 @@
 
 int main()
 {
+    tAVL *myTree;
+    tinsert(&myTree, 8);
+    tinsert(&myTree, 5);
+    tinsert(&myTree, 2);
+    tinsert(&myTree, 40);
+    tinsert(&myTree, 90);
+
+    printf("Tree (In Order): ");
+    tprint(myTree);
+    printf("\nHeight: %d", getHeight(myTree));
+    printf("\nAll levels: \n");
+    tprintAllLevels(myTree);
+    int pause; scanf("%d", &pause);
     return 0;
 }
--- tAVL.h
+++ tAVL.h
@@ -5,8 +5,8 @@ typedef struct tAVL
 {
     int data;
     int BF; //Balancing Factor
-    tAVL *left;
-    tAVL *right;
+    struct tAVL *left;
+    struct tAVL *right;
 } tAVL;
 
 //Prints in order
@@ -19,3 +19,71 @@ void tprint(tAVL* t)
         tprint(t->right);
     }
 }
+
+int getHeight(tAVL *t)
+{
+    if(t != NULL)
+    {
+        int heightLeft, heightRight;
+        heightLeft = getHeight(t->left);
+        heightRight = getHeight(t->right);
+        if(heightLeft > heightRight) return heightLeft + 1;
+        else return heightRight + 1;
+    }
+    return 0;
+}
+
+//Prints a specific level
+void tprintLevel(tAVL *t, int level, int levelOfT)
+{
+    if(t != NULL)
+    {
+        if(levelOfT == level) printf("%d ", t->data);
+        else
+        {
+            tprintLevel(t->left, level, levelOfT + 1);
+            tprintLevel(t->right, level, levelOfT + 1);
+        }
+    }
+}
+
+//Prints all the levels of a given tree
+void tprintAllLevels(tAVL *t)
+{
+    int height = getHeight(t);
+    for(int i = 0; i < height; i++)
+    {
+        tprintLevel(t, i, 0);
+        printf("\n");
+    }
+}
+
+//Inserts in Tree
+tAVL *tinsertR(tAVL *t, int value, int currentBF)
+{
+    //Current BF will hold -1, 0, +1, regarding the difference in height between the right and left subtrees (right - left)
+
+    //Found where to insert leaf
+    if(t == NULL)
+    {
+        //Create Left
+        tAVL *leaf = (tAVL*)malloc(sizeof(tAVL));
+        leaf->data = value;
+        leaf->left = NULL; 
+        leaf->right = NULL;
+        leaf->BF = currentBF;
+        //Instead of returning the node as it is, return (to father) the new node inserted.
+        return leaf;
+    }
+    //Go right
+    else if(value > t->data) t->right = tinsertR(t->right, value, currentBF);
+    //Go left
+    else t->left = tinsertR(t->left, value, currentBF);
+
+    //End of path (return itself to node father)
+    return t;
+
+}
+
+//Void Format
+void tinsert(tAVL **t, int value){*t = tinsertR(*t, value, 0);}
