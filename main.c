6f3a4923816f91564e1537b03fd79fdaadcb6e51
Guilherme <guirque@gmail.com>
2023-05-12 21:34:09 -0300

    Adding main and header file

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include "tAVL.h"
+
+int main()
+{
+    return 0;
+}
---
+++ tAVL.h
@@ -0,0 +1,18 @@
+typedef struct tAVL
+{
+    int data;
+    int BF; //Balancing Factor
+    tAVL *left;
+    tAVL *right;
+} tAVL;
+
+//Prints in order
+void tprint(tAVL* t)
+{
+    if(t != NULL)
+    {
+        tprint(t->left);
+        printf("%d ", t->data);
+        tprint(t->right);
+    }
+}
